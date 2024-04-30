b8423a007b403c78b462d96b4b279e2be205981a
Woody <jo@Woodys-MacBook-Air.local>
2024-04-30 18:53:25 +0900

    기본 세팅 git ignore, gps, image, localization, theme

---
+++ tree/android/app/src/main/kotlin/com/example/td_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.td_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tree/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  ThemeMode _themeMode = ThemeMode.system; // system은 dark, light 둘다 사용
+
+  _changeThemeMode(ThemeMode themeMode) {
+    _themeMode = themeMode;
+    setState(() {});
+  }
+
+  static const seedColor = Colors.deepPurple;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      // 언어 설정
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'), // '언어', '지역이름'
+        Locale('en', 'US'),
+      ],
+      title: 'Flutter Demo',
+      themeMode: _themeMode,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        colorSchemeSeed: seedColor
+      ),
+
+
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        colorSchemeSeed: seedColor
+      ),
+      // function도 보낼 수 있다.
+      
+      home: Home(),
+    );
+  }
+}

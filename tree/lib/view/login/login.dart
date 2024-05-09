c77d2596eee614d3da3a67073d94f631de8ca2c8
Woody <jo@Woodys-MacBook-Air.local>
2024-05-09 18:02:08 +0900

    firebase 삭제

--- tree/lib/view/login/login.dart
+++ tree/lib/view/login/login.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -92,228 +91,213 @@ class Login extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Center(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-                  .collection("user")
-                  .snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator(),);
-            }
-
-            // Func Email & Password Check 
-            checkEmailPassword(snapshot);
-
-            return Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                  
-                // Main Logo Image
-                Padding(
-                  padding: const EdgeInsets.all(40.0),
-                  child: Image.asset(
-                    "images/tree.png",
-                    width: 140,
-                    height: 140,
-                  ),
-                ),
-                // Main Logo Image
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+              
+            // Main Logo Image
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Image.asset(
+                "images/tree.png",
+                width: 140,
+                height: 140,
+              ),
+            ),
+            // Main Logo Image
+        
+            // 로고 이미지와 tf 필드의 공간 만들기
+            const SizedBox(
+              height: 60,
+            ),
+              
+              
+            // App Title
+            // const Padding(
+            //   padding: EdgeInsets.all(15.0),
+            //   child: Text(
+            //     "트리",
+            //     style: TextStyle(
+            //       fontSize: 23
+            //     ),
+            //   ),
+            // ),
+            // App Title
             
-                // 로고 이미지와 tf 필드의 공간 만들기
-                const SizedBox(
-                  height: 60,
-                ),
-                  
-                  
-                // App Title
-                // const Padding(
-                //   padding: EdgeInsets.all(15.0),
-                //   child: Text(
-                //     "트리",
-                //     style: TextStyle(
-                //       fontSize: 23
-                //     ),
-                //   ),
-                // ),
-                // App Title
-                
-            
-                // 렌더링 될때 height가 자동으로 조절된다.
-                // 이것은 TextFormField 가 SingleLineTextField를 상속받기 때문이다.
-                // Email Textfield
-                SizedBox(
-                  width: 280,
-                  height: 60,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(15,0,15,0),
-                    // 이게 없으면 밑에 표시가 나오지 않음
-                    child: Form(
-                      // 이게 없으면 밑에 표시가 나오지 않음
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      child: TextFormField(
-                        controller: emailController,
-                        focusNode: _emailFocus,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black
-                            )
-                          ),
-                          hintText: "이메일",
-                        ).copyWith(
-                          contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
-                        ),
-                        validator: (value) => CheckValidate().validateEmail(value ?? ""),
+        
+            // 렌더링 될때 height가 자동으로 조절된다.
+            // 이것은 TextFormField 가 SingleLineTextField를 상속받기 때문이다.
+            // Email Textfield
+            SizedBox(
+              width: 280,
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15,0,15,0),
+                // 이게 없으면 밑에 표시가 나오지 않음
+                child: Form(
+                  // 이게 없으면 밑에 표시가 나오지 않음
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: TextFormField(
+                    controller: emailController,
+                    focusNode: _emailFocus,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black
+                        )
                       ),
+                      hintText: "이메일",
+                    ).copyWith(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
                     ),
+                    validator: (value) => CheckValidate().validateEmail(value ?? ""),
                   ),
                 ),
-                // Email Textfield
-                  
-                  
-                // PW Textfield
-                SizedBox(
-                  width: 280,
-                  height: 120,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(15, 10,15,0),
-                    // 이게 없으면 밑에 표시가 나오지 않음
-                    child: Form(
-                      // 이게 없으면 밑에 표시가 나오지 않음
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      child: TextFormField(
-                        controller: pwController,
-                        focusNode: _pwFocus,
-                        keyboardType: TextInputType.visiblePassword,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black
-                            )
-                          ),
-                          hintText: "비밀번호",
-                        ).copyWith(
-                          contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
-                        ),
-                        validator: (value) => CheckValidate().validatePassword(_pwFocus, value!),
+              ),
+            ),
+            // Email Textfield
+              
+              
+            // PW Textfield
+            SizedBox(
+              width: 280,
+              height: 120,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10,15,0),
+                // 이게 없으면 밑에 표시가 나오지 않음
+                child: Form(
+                  // 이게 없으면 밑에 표시가 나오지 않음
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: TextFormField(
+                    controller: pwController,
+                    focusNode: _pwFocus,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black
+                        )
                       ),
+                      hintText: "비밀번호",
+                    ).copyWith(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 12.0),
                     ),
+                    validator: (value) => CheckValidate().validatePassword(_pwFocus, value!),
                   ),
                 ),
-                // PW Textfield
-                  
-                  
+              ),
+            ),
+            // PW Textfield
+              
+              
 
 
-                // Login Button
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: InkWell(
-                    onTap: () async{
-                      // await authService.signInWithGoogle();
-                      String test;
-                      
-                      if (checkEmailPw) {
-                        await box.write("infoBox", willSendDatas);
-                        Get.to(const Start());
+            // Login Button
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () async{
+                  // await authService.signInWithGoogle();
+                  String test;
+                  
+                  if (checkEmailPw) {
+                    await box.write("infoBox", willSendDatas);
+                    Get.to(const Start());
 
-                        print("succesfully in");
-                      }else{
-                        print("틀렷다~~수정해라");
-                      }
-                    },
-                    child: Container(
-                      color: const Color.fromRGBO(3, 172, 19, 1),
-                      width: 200,
-                      height: 50,
-                      child: const Padding(
-                        padding: EdgeInsets.fromLTRB(10,0,0,0),
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(0,12,10,12),
-                          child: Text(
-                            "로그인",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
+                    print("succesfully in");
+                  }else{
+                    print("틀렷다~~수정해라");
+                  }
+                },
+                child: Container(
+                  color: const Color.fromRGBO(3, 172, 19, 1),
+                  width: 200,
+                  height: 50,
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(10,0,0,0),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0,12,10,12),
+                      child: Text(
+                        "로그인",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
                         ),
+                        textAlign: TextAlign.center,
                       ),
                     ),
                   ),
                 ),
-                // Login Button
-                  
-                // Google InkWell
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: InkWell(
-                    onTap: () async {
-                      // await authService.signInWithGoogle();
-                      print("clicked Google Logo");
-                    },
-                    child: Container(
-                      color: Colors.grey[200],
-                      width: 200,
-                      height: 50,
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(30,0,0,0),
-                            child: Image.asset(
-                              "images/googleLogo.png",
-                              height: 25,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                      
-                          const Padding(
-                            padding: EdgeInsets.fromLTRB(10,0,0,0),
-                            child: Text(
-                              "구글 로그인",
-                              style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 20
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                // Google InkWell
-                  
-                // Naver InkWell
-                InkWell(
-                  onTap: () async {
-                    // await authService.signInWithGoogle();
-                    print("clicked Naver Logo");
-                  },
+              ),
+            ),
+            // Login Button
+              
+            // Google InkWell
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () async {
+                  // await authService.signInWithGoogle();
+                  print("clicked Google Logo");
+                },
+                child: Container(
+                  color: Colors.grey[200],
+                  width: 200,
+                  height: 50,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30,0,0,0),
+                        child: Image.asset(
+                          "images/googleLogo.png",
+                          height: 25,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
                   
-                      Image.asset(
-                        "images/naverLogo.png",
-                        width: 200,
-                        height: 50,
-                        fit: BoxFit.cover,
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(10,0,0,0),
+                        child: Text(
+                          "구글 로그인",
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 20
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
-                // Naver InkWell
-                  
-                  
-              ],
-            );
-          },
-          
-        ),
+              ),
+            ),
+            // Google InkWell
+              
+            // Naver InkWell
+            InkWell(
+              onTap: () async {
+                // await authService.signInWithGoogle();
+                print("clicked Naver Logo");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+              
+                  Image.asset(
+                    "images/naverLogo.png",
+                    width: 200,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ),
+            // Naver InkWell
+              
+              
+          ],
+        )
       ),
     );
   }
--- tree/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tree/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,23 +5,13 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
 import file_selector_macos
-import firebase_analytics
-import firebase_core
-import firebase_database
-import firebase_storage
 import geolocator_apple
 import google_sign_in_ios
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
-  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
